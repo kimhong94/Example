d79256fbcaa5208787c403651ba28c904ecdaa42
khs <kimhong94@naver.com>
2017-07-16 17:25:30 +0900

    example

---
+++ ex04_FileUpload/src/test/java/com/hong/controller/UploadControllerTest.java
@@ -0,0 +1,21 @@
+package com.hong.controller;
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import org.junit.Test;
+
+public class UploadControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		List<String> list = new LinkedList<>();
+		Queue<String> queue = new LinkedList<>();
+		
+	}
+
+}
