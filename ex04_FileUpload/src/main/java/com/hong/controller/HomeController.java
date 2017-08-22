a4a1aafdb1306e7a8983c5d6886989be7470a2e1
khs <kimhong94@naver.com>
2017-08-23 00:28:58 +0900

    master2

--- ex04_FileUpload/src/main/java/com/hong/controller/HomeController.java
+++ ex04_FileUpload/src/main/java/com/hong/controller/HomeController.java
@@ -31,6 +31,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 * master init
+	 * master2
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
